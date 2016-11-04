6e9d09e215184d16ee4c0e11705c0f830ff0dd3b
Jonathan Paz <Jonathan@paztop.com>
2016-11-03 20:53:57 -0500

    Updated model

--- src/main/resources/com/pazdev/authserver/model/database_ddl.sql
+++ src/main/resources/com/pazdev/authserver/model/database_ddl.sql
@@ -20,160 +20,165 @@
 
 drop table if exists public.uploaded_content cascade;
 create table public.uploaded_content (
-   id serial primary key,
-   public_id uuid not null,
-   contents bytea not null,
-   mime_type text not null
+    id serial primary key,
+    public_id uuid not null,
+    contents bytea not null,
+    mime_type text not null
 );
 drop table if exists public.profile cascade;
 CREATE TABLE public.profile
 (
-   id serial PRIMARY KEY, 
-   sub text UNIQUE NOT NULL, 
-   preferred_username text NOT NULL, 
-   password_bytes bytea NOT NULL, 
-   salt bytea NOT NULL, 
-   rounds integer NOT NULL,
-   picture integer references uploaded_content (id) on delete set null, 
-   website text, 
-   email text, 
-   email_verified boolean NOT NULL DEFAULT false, 
-   gender text, 
-   birthdate date, 
-   zoneinfo text, 
-   locale text, 
-   phone_number text, 
-   phone_number_verified boolean NOT NULL DEFAULT false, 
-   updated_at timestamp with time zone
+    id serial PRIMARY KEY, 
+    sub text UNIQUE NOT NULL, 
+    password_bytes bytea NOT NULL, 
+    salt bytea NOT NULL, 
+    rounds integer NOT NULL,
+    profile_name text,
+    given_name text,
+    family_name text,
+    middle_name text,
+    nickname text,
+    preferred_username text NOT NULL, 
+    picture integer references uploaded_content (id) on delete set null, 
+    website text, 
+    email text, 
+    email_verified boolean NOT NULL DEFAULT false, 
+    gender text, 
+    birthdate date, 
+    zoneinfo text, 
+    locale text, 
+    phone_number text, 
+    phone_number_verified boolean NOT NULL DEFAULT false, 
+    updated_at timestamp
 )
 ;
 drop table if exists public.profile_address cascade;
 create table public.profile_address (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   formatted text, 
-   street_address text,
-   locality text,
-   region text,
-   postal_code text,
-   country text, 
-   address_lang text,
-   unique(profile_id, address_lang)
-);
-drop table if exists public.profile_family_name cascade;
-create table public.profile_family_name (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   profile_family_name text not null,
-   profile_family_name_lang text,
-   unique (profile_id, profile_family_name_lang)
-);
-drop table if exists public.profile_given_name cascade;
-create table public.profile_given_name (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   profile_given_name text not null,
-   profile_given_name_lang text,
-   unique (profile_id, profile_given_name_lang)
-);
-drop table if exists public.profile_middle_name cascade;
-create table public.profile_middle_name (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   profile_middle_name text not null,
-   profile_middle_name_lang text,
-   unique (profile_id, profile_middle_name_lang)
-);
-drop table if exists public.profile_nickname cascade;
-create table public.profile_nickname (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   profile_nickname text not null,
-   profile_nickname_lang text,
-   unique (profile_id, profile_nickname_lang)
-);
-drop table if exists public.profile_name cascade;
-create table public.profile_name (
-   id serial primary key,
-   profile_id integer references public.profile (id) on delete cascade,
-   profile_name text not null,
-   profile_name_lang text,
-   unique (profile_id, profile_name_lang)
+    id serial primary key,
+    profile_id integer references public.profile (id) on delete cascade,
+    formatted text, 
+    street_address text,
+    locality text,
+    region text,
+    postal_code text,
+    country text
 );
 drop table if exists public.profile_attribute cascade;
 create table public.profile_attribute (
-   id serial primary key,
-   account_id integer references public.profile (id),
-   attribute_name text not null,
-   attribute_value text,
-   unique (account_id, attribute_name)
-)
-;
+    id serial primary key,
+    account_id integer references public.profile (id),
+    attribute_name text not null,
+    attribute_value text,
+    unique (account_id, attribute_name)
+);
 drop table if exists public.client cascade;
 create table public.client (
-   id serial primary key,
-   client_id text not null unique,
-   client_secret text not null unique,
-   client_type text not null,
-   issue_date timestamp with time zone not null,
-   grant_types text not null,
-   response_types text not null,
-   scopes text,
-   software_id uuid,
-   software_version text
-)
-;
-drop table if exists public.client_name cascade;
-create table public.client_name (
-   id serial primary key,
-   client_id integer references public.client(id) on delete cascade,
-   client_name text not null,
-   client_lang text,
-   unique (client_id, client_lang)
-);
-drop table if exists public.client_policy cascade;
-create table public.client_policy (
-   id serial primary key,
-   client_id integer references public.client(id) on delete cascade,
-   policy_uri text not null,
-   policy_uri_lang text,
-   unique (client_id, policy_uri_lang)
-);
-drop table if exists public.client_logo cascade;
-create table public.client_logo (
-   id serial primary key,
-   client_id integer references public.client(id) on delete cascade,
-   logo_image integer references uploaded_content(id) on delete cascade not null,
-   logo_lang text,
-   unique (client_id, logo_lang)
-);
-drop table if exists public.client_terms cascade;
-create table public.client_terms (
-   id serial primary key,
-   client_id integer references public.client(id) on delete cascade,
-   terms_uri text not null,
-   terms_uri_lang text,
-   unique (client_id, terms_uri_lang)
-);
-drop table if exists public.client_uri cascade;
-create table public.client_uri (
-   id serial primary key,
-   client_id integer references public.client(id) on delete cascade,
-   client_uri text not null,
-   client_uri_lang text,
-   unique (client_id, client_uri_lang)
+    id serial primary key,
+    client_type text not null,
+    client_id text not null unique,
+    client_secret text not null unique,
+    client_id_issued_at timestamp,
+    client_secret_expires_at timestamp,
+    token_endpoint_auth_method text,
+    client_name text,
+    client_uri text,
+    logo_uri text,
+    scopes text,
+    tos_uri text,
+    policy_uri text,
+    issue_date timestamp not null,
+    software_id uuid,
+    software_version text,
+    application_type text,
+    sector_identifier_uri text,
+    subject_type text,
+    id_token_signed_response_alg text,
+    id_token_encrypted_response_alg text,
+    userinfo_signed_response_alg text,
+    userinfo_encrypted_response_alg text,
+    request_object_signing_alg text,
+    request_object_encryption_alg text,
+    token_endpoint_auth_signing_method text,
+    default_max_age integer,
+    require_auth_time boolean,
+    initiate_login_uri text
 );
 drop table if exists public.client_redirect_uri cascade;
 create table public.client_redirect_uri (
-   id serial primary key,
-   client_id integer not null references public.client (id) on delete cascade,
-   redirect_uri text not null,
-   unique(client_id, redirect_uri)
+    id serial primary key,
+    client_id integer not null references public.client (id) on delete cascade,
+    redirect_uri text not null,
+    unique(client_id, redirect_uri)
+);
+drop table if exists public.client_grant_type cascade;
+create table public.client_grant_type (
+    id serial primary key,
+    client_id integer references public.client(id) on delete cascade,
+    grant_type text not null,
+    unique (client_id, grant_type)
+);
+drop table if exists public.client_response_type cascade;
+create table public.client_response_type (
+    id serial primary key,
+    client_id integer references public.client(id) on delete cascade,
+    response_type text not null,
+    unique (client_id, response_type)
 );
 drop table if exists public.client_contact cascade;
 create table public.client_contact (
-   id serial primary key,
-   client_id integer not null references public.client (id) on delete cascade,
-   personal_name text,
-   email_address text not null
+    id serial primary key,
+    client_id integer not null references public.client (id) on delete cascade,
+    personal_name text,
+    email_address text not null
+);
+drop table if exists public.client_default_acr_value cascade;
+create table public.client_default_acr_value (
+    id serial primary key,
+    client_id integer not null references public.client (id) on delete cascade,
+    default_acr_value text not null,
+    unique (client_id, default_acr_value)
+);
+drop table if exists public.token cascade;
+create table public.token (
+    id serial primary key,
+    issuer text,
+    subject text,
+    audience text,
+    expiration_time timestamp,
+    not_before timestamp,
+    issued_at timestamp not null,
+    jwt_id text unique not null
+);
+drop table if exists public.token_extra_claim cascade;
+create table public.token_extra_claim (
+    id serial primary key,
+    token_id integer not null references public.token(id) on delete cascade,
+    claim_name text not null,
+    claim_value text not null,
+    unique(token_id,claim_name)
+);
+drop table if exists public.o_auth2_token cascade;
+create table public.o_auth2_token (
+    id integer references public.token (id),
+    scopes text,
+    client_id text,
+    username text,
+    token_type text
+);
+drop table if exists public.id_token cascade;
+create table public.id_token (
+    id integer references public.token (id),
+    auth_time timestamp,
+    nonce text,
+    acr text,
+    azp text,
+    atHash text,
+    cHash text
+);
+drop table if exists public.id_token_amr cascade;
+create table public.id_token_amr (
+    id serial primary key,
+    token_id integer references public.token (id) on delete cascade,
+    amr text not null,
+    unique (token_id, amr)
 );
